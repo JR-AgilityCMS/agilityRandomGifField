0ff9e016116e6da40616a92ce8eba4ed7fa6e84c
James Rao <55598464+JamesRao98@users.noreply.github.com>
2021-12-13 13:12:23 -0500

    using the sdk now

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const {GiphyFetch} = require("@giphy/js-fetch-api");
+
 const el = id => document.getElementById(id);
-const getRandomGif = async apiKey => {
-  let resp = await fetch("https://api.giphy.com/v1/gifs/random", {headers: {apikey: apiKey}});
-  let json = await resp.json();
-  return json.data.url;
-}
+
+// const getRandomGif = async apiKey => {
+//   let resp = await fetch("https://api.giphy.com/v1/gifs/random", {headers: {apikey: apiKey}});
+//   let json = await resp.json();
+//   return json.data.url;
+// }
 
 //standard app config
 let appConfig = {
@@ -39,9 +42,8 @@ if(componentToRender === 'AppConfig') {
       let imgElem = el("imgElem");
       let regenButt = el("regenButt");
 
-      let giphyApiKey = sdk.configValues.giphyApiKey;
-
-      getRandomGif(giphyApiKey).then(url => imgElem.src = url);
+      const gf = new GiphyFetch(sdk.configValues.giphyApiKey)
+      gf.random().then(resp => imgElem.src = JSON.stringify(resp));
 
       regenButt.onclick = () => {
         getRandomGif(giphyApiKey).then(url => imgElem.src = url);
